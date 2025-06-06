46f285c3e52cd3c709404f5d0b0c9bd8a93a4181
Naveen9712 <kandakurthinaveen97@gmail.com>
2025-06-06 18:01:48 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DashboardCard from './components/DashboardCard';
+
+const App: React.FC = () => {
+  return (
+    <DashboardCard />
+  );
+};
+
+export default App;
